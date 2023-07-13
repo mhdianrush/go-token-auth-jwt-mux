b14eb1fe74464de5e7fee0f2f2031bc4ec406703
mhdianrush <mhdianrush@gmail.com>
2023-07-13 10:19:43 +0700

    Created for Login, Register, and Logout Function

---
+++ controllers/auth.controller.go
@@ -0,0 +1,15 @@
+package controllers
+
+import "net/http"
+
+func Login(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+
+}
