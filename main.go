a16ccaaf08e6070e395e4f86b808326be806d04e
mhdianrush <mhdianrush@gmail.com>
2023-07-14 21:35:19 +0700

    Created for New Route

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/mhdianrush/go-token-auth-jwt-mux/config"
 	"github.com/mhdianrush/go-token-auth-jwt-mux/controllers"
+	"github.com/mhdianrush/go-token-auth-jwt-mux/middlewares"
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,6 +20,14 @@ func main() {
 	r.HandleFunc("/register", controllers.Register).Methods(http.MethodPost)
 	r.HandleFunc("/logout", controllers.Logout).Methods(http.MethodGet)
 
+	// this route will be protect with middleware
+	// if user isn't login, it will return an unauthorized response.
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/products", controllers.Index).Methods(http.MethodGet)
+
+	// use middleware
+	api.Use(middlewares.JWTMiddleware)
+
 	logger := logrus.New()
 
 	file, err := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
@@ -30,7 +39,7 @@ func main() {
 	logger.Println("Server Running on Port 8080")
 
 	server := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: r,
 	}
 	err = server.ListenAndServe()
