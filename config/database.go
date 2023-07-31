df901ee62876dca97d770f2b63dfb43ecf6dd3c3
mhdianrush <mhdianrush@gmail.com>
2023-07-31 12:48:08 +0700

    Developed for Using godotenv Package to Read env File that Used in Open Connection to MySQL Database

--- config/database.go
+++ config/database.go
@@ -1,24 +1,35 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/mhdianrush/go-token-auth-jwt-mux/entities"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var logger = logrus.New()
 var DB *gorm.DB
 
 func ConnectDB() {
-	logger := logrus.New()
+	if err := godotenv.Load(); err != nil {
+		logger.Printf("failed load env file %s", err.Error())
+	}
 
-	db, err := gorm.Open(mysql.Open("root:admin@tcp(127.0.0.1:3306)/go_token_auth_jwt_mux?parseTime=true"), &gorm.Config{})
+	dataSourceName := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("DATABASE_NAME"),
+	)
+	db, err := gorm.Open(mysql.Open(dataSourceName), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		logger.Printf("failed connect to database %s", err.Error())
 	}
 	db.AutoMigrate(&entities.User{})
 
-	logger.Println("Database Connected")
-
 	DB = db
+
+	logger.Println("database connected")
 }
